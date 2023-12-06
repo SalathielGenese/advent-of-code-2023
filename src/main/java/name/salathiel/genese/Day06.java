97d7e677bbff0604b1821e7a041f364f1699c831
Salathiel <salathiel@genese.name>
2023-12-06 07:03:52 +0100

    D6C2

--- src/main/java/name/salathiel/genese/Day06.java
+++ src/main/java/name/salathiel/genese/Day06.java
@@ -1,49 +1,63 @@
 package name.salathiel.genese;
 
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.math.BigInteger;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.IntStream;
 
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.valueOf;
 import static java.util.Objects.requireNonNull;
 
 public class Day06 {
     public static void main(String[] args) {
-        long product = 1;
-        int[] times = null, distances = null;
+//        long product = 1;
+//        int[] times = null, distances = null;
+        BigInteger[] times = null, distances = null;
         final var loader = Day06.class.getClassLoader();
 
         try (final var scanner = new Scanner(requireNonNull(loader.getResourceAsStream("day-06-input.txt")))) {
             while (scanner.hasNext()) {
                 final var next = scanner.nextLine();
                 if (next.startsWith("Time:")) {
-                    times = Arrays.stream(next.split(":")[1].strip().split("\\s+"))
-                            .mapToInt(Integer::parseInt)
-                            .toArray();
+//                    times = Arrays.stream(next.split(":")[1].strip().split("\\s+"))
+//                            .mapToInt(Integer::parseInt)
+//                            .toArray();
+                    times = new BigInteger[]{new BigInteger(next.split(":")[1].replaceAll("\\s+", ""))};
                 } else if (next.startsWith("Distance:")) {
-                    distances = Arrays.stream(next.split(":")[1].strip().split("\\s+"))
-                            .mapToInt(Integer::parseInt)
-                            .toArray();
+//                    distances = Arrays.stream(next.split(":")[1].strip().split("\\s+"))
+//                            .mapToInt(Integer::parseInt)
+//                            .toArray();
+                    distances = new BigInteger[]{new BigInteger(next.split(":")[1].replaceAll("\\s+", ""))};
                 }
             }
         }
 
-        for (var i = 0; i < times.length; i++) {
-            product *= getRecordChargeTimes(times[i], distances[i]).size();
-        }
+//        for (var i = 0; i < times.length; i++) {
+//            product *= getRecordChargeTimes(times[i], distances[i]).size();
+//        }
 
-        System.out.println(product);
+//        System.out.println(product);
+        System.out.println(getRecordChargeTimes(times[0], distances[0]));
     }
 
-    static Set<Integer> getRecordChargeTimes(int time, int distance) {
-        final var chargeTimes = new LinkedHashSet<Integer>();
+//    static Set<Integer> getRecordChargeTimes(int time, int distance) {
+//        final var chargeTimes = new LinkedHashSet<Integer>();
+//
+//        for (int i = 1; i < time; i++) {
+//            if (i * (time - i) > distance) {
+//                chargeTimes.add(i);
+//            }
+//        }
+//        return chargeTimes;
+//    }
 
-        for (int i = 1; i < time; i++) {
-            if (i * (time - i) > distance) {
-                chargeTimes.add(i);
+    static BigInteger getRecordChargeTimes(BigInteger time, BigInteger distance) {
+        var count = BigInteger.ZERO;
+
+        for (BigInteger i = valueOf(1); i.compareTo(time) <= 0; i = i.add(ONE)) {
+            if (i.multiply(time.subtract(i)).compareTo(distance) > 0) {
+                count = count.add(ONE);
             }
         }
-        return chargeTimes;
+        return count;
     }
 }
